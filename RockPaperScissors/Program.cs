832957e69b421e49160194b074576eee6fcc1b8f
SmollBoiPEnguin <63251618+SmollBoiPEnguin@users.noreply.github.com>
2020-05-28 15:25:48 -0500

    Initial commit

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.WriteLine("Choose Rock, Paper, or Scissors");
+            inputPlayer = Console.ReadLine();
+            inputPlayer = inputPlayer.ToUpper();
+
+            Random rnd = new Random();
+            randomInt = rnd.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "Rock";
+                    Console.WriteLine("Computer chose Rock");
+                    if (playerInput == "Rock")
+                    {
+                        Console.WriteLine("Draw!\n\n");
+                    }
+                    else if (inputPlayer == "Paper")
+                    {
+                        Console.WriteLine("Player Wins\n\n");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "Scissors");
+                    {
+                        Console.WriteLine("CPU wins! \n\n");
+                        scoreCPU++;
+
+                    }
+                    break;
+                case 2:
+                    inputCPU = "Paper";
+                    if (playerInput == "Rock")
+                    {
+                        Console.Writeline("CPU won\n\n");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "Paper");
+                    {
+                        Console.Writeline("Draw!\n\n");
+                    }
+                    else if (inputPlayer == "Scissors") ;
+                    {
+                        Console.Writeline("Player wins!\n\n");
+                    break;
+                case 3:
+                    inputCPU = "Scissors";
+                    break;
+                default:
+                    Console.WriteLine("Invalid Entry");
+                    break;
+            }
+        }
+    }
+}
