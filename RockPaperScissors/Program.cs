4eae50dc1c56ff2def53d3d08bb6ee027b1d21e8
SmollBoiPEnguin <63251618+SmollBoiPEnguin@users.noreply.github.com>
2020-06-05 12:36:33 -0500

    Finished game.

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -11,15 +11,29 @@ class Program
         Rock, Paper, Scissors
     }
 
+    enum Outcome
+    {
+        Win, Lose, Draw
+    }
+
     static void Main(string[] args)
     {
         bool keepPlaying = true;
 
         while (keepPlaying)
         {
-            GetPlayerInput();
-            
-            keepPlaying = false;
+            Outcome roundOutcome = GetGameOutcome(GetPlayerInput(), GetComputerInput());
+            if(roundOutcome == Outcome.Win)
+            {
+                Console.WriteLine("you win\n");
+            }else if(roundOutcome == Outcome.Lose)
+            {
+                Console.WriteLine("you lose\n");
+            }
+            else if (roundOutcome == Outcome.Draw)
+            {
+                Console.WriteLine("draw\n");
+            }
         }
     }
 
@@ -32,44 +46,71 @@ class Program
             Console.WriteLine("Select: Rock, Paper, or Scissors");
             string playerInput = Console.ReadLine();
 
-            if (playerInput.ToLower() == "rock")
+            if (playerInput.Equals("rock" , StringComparison.OrdinalIgnoreCase))
             {
                 toReturn = Selection.Rock;
                 validInput = true;
             }
-            else if (playerInput.ToLower() == "paper")
+            else if (playerInput.Equals("paper", StringComparison.OrdinalIgnoreCase))
             {
                 toReturn = Selection.Paper;
                 validInput = true;
             }
-            else if (playerInput.ToLower() == "scissors")
+            else if (playerInput.Equals("scissors", StringComparison.OrdinalIgnoreCase))
             {
                 toReturn = Selection.Scissors;
                 validInput = true;
+            }   
+            else if (playerInput.Equals("Quit", StringComparison.OrdinalIgnoreCase))
+            {
+                Environment.Exit(0);
             }
             else
             {
-                Console.WriteLine("Entry invalid");
-            }
-            
-            
-            if (ComputerInput.ToLower() == "rock")
-            {
-                toReturn = Selection.Rock;
-                validInput = true;
-            }
-            else if (ComputerInput.ToLower() == "paper")
-            {
-                toReturn = Selection.Paper;
-                validInput = true;
-            }
-            else if (ComputerInput.ToLower() == "scissors")
-            {
-                toReturn = Selection.Scissors;
-                validInput = true;
+                Console.WriteLine("Entry invalid\n");
             }
         }
         return toReturn;
     } 
-        
+
+    static Selection GetComputerInput()
+    {
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(0, 3);
+        Selection selection = (Selection) randomNumber;
+        Console.WriteLine("Computer selected " + selection);
+        return selection;
+    }
+
+    static Outcome GetGameOutcome(Selection player, Selection computer)
+    {
+        if  (player == Selection.Paper && computer == Selection.Rock)
+        {
+            return Outcome.Win;
+        }
+        else if (player == Selection.Rock && computer == Selection.Scissors)
+        {
+            return Outcome.Win;
+        }
+        else if (player == Selection.Scissors && computer == Selection.Paper)
+        {
+            return Outcome.Win;
+        }
+        else if (player == Selection.Scissors && computer == Selection.Rock)
+        {
+            return Outcome.Lose;
+        }
+        else if (player == Selection.Paper && computer == Selection.Scissors)
+        {
+            return Outcome.Lose;
+        }
+        else if (player == Selection.Rock && computer == Selection.Paper)
+        {
+            return Outcome.Lose;
+        }
+        else
+        {
+            return Outcome.Draw;
+        }
+    }
 }
